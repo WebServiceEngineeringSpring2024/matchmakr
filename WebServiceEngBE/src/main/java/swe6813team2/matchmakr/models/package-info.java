0d17422f7e900e2c4b7b23f4b4969f443f5bf51b
Reed McCallum <donald.mccallum20@t-mobile.com>
2024-03-19 14:17:02 -0400

    move FE and BE into submodules

--- WebServiceEngBE/src/main/java/swe6813team2/matchmakr/models/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This module contains the web app's models
- */
-/**
- * 
- */
-package swe6813team2.matchmakr.models;
--- WebServiceEngBE/src/main/java/swe6813team2/matchmakr/repositories/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This module contains code for the JPA repository, which manages the data in the web app
- */
-/**
- * 
- */
-package swe6813team2.matchmakr.repositories;
